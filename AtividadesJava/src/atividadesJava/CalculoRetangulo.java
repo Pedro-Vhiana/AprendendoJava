d61947eda9cb6df399806fdba195b5f2bc3db72c
pedro <pedro@192.168.0.236>
2023-10-29 15:13:42 -0300

    Atividades feitas em java 29.10.2023

---
+++ AtividadesJava/src/atividadesJava/CalculoRetangulo.java
@@ -0,0 +1,17 @@
+package atividadesJava;
+
+public class CalculoRetangulo {
+	
+	public static void main(String[] args) {
+	
+	int area1 = 5;
+	
+	int area2 = 5;
+	
+	int resultado = (area1 + area2) * 2;
+	
+	System.out.printf("área do retangulo: " + resultado );
+	
+	}
+
+}
---
+++ AtividadesJava/src/atividadesJava/OrcamentoConstrutora.java
@@ -0,0 +1,27 @@
+package atividadesJava;
+
+public class OrcamentoConstrutora {
+	
+	public static void main(String[] args) {
+		
+		double larg =  6;
+		
+		double profundi = 1.80;
+		
+		double compri = 10;
+		
+		double preco = (larg * profundi * compri) * 100;
+		
+		System.out.println("O preço da piscina mediante a cubagem é "+"R$"+ preco);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
